877ece324cee6c0d315cb697ec072f633a5d60af
ectechno <hasithy@99x.lk>
2014-11-26 10:24:24 +0530

    Update SendMail.cs

--- CustomerFeedback/CustomerFeedback/Home/RequestForm/SendMail.cs
+++ CustomerFeedback/CustomerFeedback/Home/RequestForm/SendMail.cs
@@ -12,11 +12,10 @@ namespace CustomerFeedback.RequestForm
     {
         public void send(string address, string subject, string message)
         {
-            NetworkCredential cred = new NetworkCredential(System.Environment.GetEnvironmentVariable("CustomerFeedbackMailAddress")
-                , System.Environment.GetEnvironmentVariable("CustomerFeedbackMailPassword"));
+            NetworkCredential cred = new NetworkCredential( "hr@99x.lk" , "admin@99x");
             MailMessage msg = new MailMessage();
             msg.To.Add(address);
-            msg.From = new MailAddress(System.Environment.GetEnvironmentVariable("CustomerFeedbackMailAddress"));
+            msg.From = new MailAddress("hr@99x.lk");
             msg.Subject = subject;
             msg.Body = message;
             SmtpClient client = new SmtpClient("mail.99xtechnology.com", 25);
