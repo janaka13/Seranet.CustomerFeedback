686cf5cf088dbd291c9710694a394c7110437e71
ectechno <hasithy@99x.lk>
2015-06-24 15:38:55 +0530

    Update SendMail.cs

--- CustomerFeedback/CustomerFeedback/Home/RequestForm/SendMail.cs
+++ CustomerFeedback/CustomerFeedback/Home/RequestForm/SendMail.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Net.Mail;
 using System.Web;
 using System.Web.Services;
-using System.Diagnostics;
+
 
 namespace CustomerFeedback.RequestForm
 {
@@ -13,8 +13,8 @@ namespace CustomerFeedback.RequestForm
     {
         public void send(string address, string subject, string message)
         {
-           EventLog _myLog = new EventLog("Application", ".", "Customer Feedback");
-            _myLog.WriteEntry("Email sending started");
+          
+           
             NetworkCredential cred = new NetworkCredential(System.Environment.GetEnvironmentVariable("CustomerFeedbackUserName")
                           , System.Environment.GetEnvironmentVariable("CustomerFeedbackMailPassword"));
 
@@ -27,7 +27,6 @@ namespace CustomerFeedback.RequestForm
             SmtpClient client = new SmtpClient(System.Environment.GetEnvironmentVariable("EmailServerAddress"), 25);
             client.Credentials = cred;
             Console.WriteLine(" User Name :" + System.Environment.GetEnvironmentVariable("CustomerFeedbackUserName") + " Password : " + System.Environment.GetEnvironmentVariable("CustomerFeedbackMailPassword") + "  Email: " + System.Environment.GetEnvironmentVariable("CustomerFeedbackMailAddress"));
-           _myLog.WriteEntry(" User Name :" + System.Environment.GetEnvironmentVariable("CustomerFeedbackUserName") + " Password : " + System.Environment.GetEnvironmentVariable("CustomerFeedbackMailPassword") + "  Email: " + System.Environment.GetEnvironmentVariable("CustomerFeedbackMailAddress") + "Email Server :" + System.Environment.GetEnvironmentVariable("EmailServerAddress"));
           
             // client.EnableSsl = true;  
             client.Send(msg);
