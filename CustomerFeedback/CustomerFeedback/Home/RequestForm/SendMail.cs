8b9377a3518bd612d38fd120ac8334a7fc50605e
Janaka Jeewantha <janaka13@gmail.com>
2016-11-15 18:00:44 +0530

    Change email sending logic to office 365 server

--- CustomerFeedback/CustomerFeedback/Home/RequestForm/SendMail.cs
+++ CustomerFeedback/CustomerFeedback/Home/RequestForm/SendMail.cs
@@ -1,34 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using System.Web;
-using System.Web.Services;
-
 
 namespace CustomerFeedback.RequestForm
 {
     public class SendMail
     {
+        public string UserName
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("CustomerFeedbackUserName");
+            }
+        }
+
+        public string UserPassword
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("CustomerFeedbackMailPassword");
+            }
+        }
+
+        public string UserMail
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("CustomerFeedbackMailAddress");
+            }
+        }
+
+        public string EmailServer
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("EmailServerAddress");
+            }
+        }
+
+        public int EmailServerPort
+        {
+            get
+            {
+                var port = Environment.GetEnvironmentVariable("EmailServerPort");
+                int portNumber;
+                if (int.TryParse(port, out portNumber))
+                {
+                    return portNumber;
+                }
+                return 25;
+            }
+        }
+
         public void send(string address, string subject, string message)
         {
-                NetworkCredential cred = new NetworkCredential(System.Environment.GetEnvironmentVariable("CustomerFeedbackUserName"), System.Environment.GetEnvironmentVariable("CustomerFeedbackMailPassword"));
+            NetworkCredential cred = new NetworkCredential(UserName, UserPassword);
 
-                MailMessage msg = new MailMessage();
-                msg.To.Add(address);
-                msg.From = new MailAddress(System.Environment.GetEnvironmentVariable("CustomerFeedbackMailAddress"));
-                msg.Subject = subject;
-                msg.Body = message;
+            MailMessage msg = new MailMessage();
+            msg.To.Add(address);
+            msg.From = new MailAddress(UserMail);
+            msg.Subject = subject;
+            msg.Body = message;
 
-                SmtpClient client = new SmtpClient(System.Environment.GetEnvironmentVariable("EmailServerAddress"), 25);
-                client.Credentials = cred;
-                Console.WriteLine(" User Name :" + System.Environment.GetEnvironmentVariable("CustomerFeedbackUserName") + " Password : " + System.Environment.GetEnvironmentVariable("CustomerFeedbackMailPassword") + "  Email: " + System.Environment.GetEnvironmentVariable("CustomerFeedbackMailAddress"));
-
-                // client.EnableSsl = true;  
-                client.Send(msg);
-                Console.WriteLine("Done");
-                
+            SmtpClient client = new SmtpClient(EmailServer, EmailServerPort);
+            client.EnableSsl = true;
+            client.Credentials = cred; 
+            client.Send(msg);
         }
     }
 }
